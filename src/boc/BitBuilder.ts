41fb7305488c9740b7c519da8e76fa534cdf9f79
Steve Korshakov <steve@korshakov.com>
2022-12-24 23:35:57 +0400

    wip: working on BitBuilder

--- src/boc/BitBuilder.ts
+++ src/boc/BitBuilder.ts
@@ -32,6 +32,60 @@ export class BitBuilder {
         }
     }
 
+    writeUint(value: bigint | number, bits: number) {
+
+        // Check input
+        let v = BigInt(value);
+        let vBits = (1n << BigInt(bits));
+        if (v < 0 || v >= vBits) {
+            throw Error(`bitLength is too small for a value ${value}. Got ${bits}`);
+        }
+
+        // Convert number to bits
+        let b: boolean[] = [];
+        while (v > 0) {
+            b.push(v % 2n === 1n);
+            v /= 2n;
+        }
+
+        // Write bits
+        for (let i = 0; i < bits; i++) {
+            let off = bits - i - 1;
+            if (off < b.length) {
+                this.writeBit(b[off]);
+            } else {
+                this.writeBit(false);
+            }
+        }
+    }
+
+    writeInt(value: bigint | number, bits: number) {
+
+        // Check input
+        let v = BigInt(value);
+        let vBits = 1n << (BigInt(bits) - 1n);
+        if (v < -vBits || v >= vBits) {
+            throw Error(`value is out of range for ${bits} bits. Got ${value}`);
+        }
+
+        // Write sign
+        if (v < 0) {
+            this.writeBit(true);
+            v = -v;
+        } else {
+            this.writeBit(false);
+        }
+
+        // Write value
+        this.writeUint(v, bits - 1);
+    }
+
+    writeBuffer(src: Buffer) {
+        for (let i = 0; i < src.length; i++) {
+            this.writeInt(src[i], 8);
+        }
+    }
+
     build() {
         return new BitString(this._buffer, 0, this._length);
     }
