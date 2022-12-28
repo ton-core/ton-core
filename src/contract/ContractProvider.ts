0d6dec551709f7f389752f03d997bb09c3e4bdd0
Steve Korshakov <steve@korshakov.com>
2022-12-29 03:32:23 +0400

    ref: update ContractProvider

--- src/contract/ContractProvider.ts
+++ src/contract/ContractProvider.ts
@@ -1,11 +1,14 @@
 import { Cell } from "../boc/Cell";
+import { SendMode } from "../messages/SendMode";
 import { TupleReader } from "../tuple/reader";
 import { TupleItem } from "../tuple/tuple";
+import { Maybe } from "../utils/maybe";
 import { AccountState } from "./AccountState";
-import { Sender } from "./Sender";
+import { Sender } from './Sender';
 
 export interface ContractProvider {
     getState(): Promise<AccountState>;
-    callGetMethod(name: string, args: TupleItem[]): Promise<{ stack: TupleReader }>;
-    send(message: Cell, via?: Sender): Promise<void>;
+    get(name: string, args: TupleItem[]): Promise<{ stack: TupleReader }>;
+    external(message: Cell): Promise<void>;
+    internal(via: Sender, args: { value: bigint | string, bounce?: Maybe<boolean>, sendMode?: SendMode, body?: Cell | string }): Promise<void>;
 }
