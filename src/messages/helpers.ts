de57f5af66988cda0ae5699fef1abcf31137be21
Steve Korshakov <steve@korshakov.com>
2022-12-29 01:13:31 +0400

    fix: helpers signatures

--- src/messages/helpers.ts
+++ src/messages/helpers.ts
@@ -8,20 +8,35 @@ import { InternalMessage } from "./InternalMessage";
 import { ExternalMessage } from './ExternalMessage';
 import { StateInit } from "./StateInit";
 import { CommentMessage } from "./CommentMessage";
+import { beginCell } from "../boc/Builder";
 
 export function internal(src: {
-    to: Address,
+    to: Address | string,
     amount: bigint,
     bounce?: Maybe<boolean>,
     init?: Maybe<{ code?: Maybe<Cell>, data?: Maybe<Cell> }>,
     body?: Cell
 }) {
+
+    // Resolve bounce
     let bounce = true;
     if (src.bounce !== null && src.bounce !== undefined) {
         bounce = src.bounce;
     }
+
+    // Resolve address
+    let to: Address;
+    if (typeof src.to === 'string') {
+        to = Address.parse(src.to);
+    } else if (Address.isAddress(src.to)) {
+        to = src.to;
+    } else {
+        throw new Error(`Invalid address ${src.to}`);
+    }
+
+    // Create message
     return new InternalMessage({
-        to: src.to,
+        to: to,
         value: src.amount,
         bounce: src.bounce || false,
         body: new CommonMessageInfo({
@@ -32,12 +47,23 @@ export function internal(src: {
 }
 
 export function external(src: {
-    to: Address,
+    to: Address | string,
     init?: Maybe<{ code?: Maybe<Cell>, data?: Maybe<Cell> }>,
     body?: Cell
 }) {
+
+    // Resolve address
+    let to: Address;
+    if (typeof src.to === 'string') {
+        to = Address.parse(src.to);
+    } else if (Address.isAddress(src.to)) {
+        to = src.to;
+    } else {
+        throw new Error(`Invalid address ${src.to}`);
+    }
+
     return new ExternalMessage({
-        to: src.to,
+        to: to,
         body: new CommonMessageInfo({
             stateInit: src.init ? new StateInit(src.init) : null,
             body: src.body ? new CellMessage(src.body) : new EmptyMessage()
@@ -46,5 +72,7 @@ export function external(src: {
 }
 
 export function comment(src: string) {
-    return new CommentMessage(src);
+    return beginCell()
+        .storeWritable(new CommentMessage(src))
+        .endCell();
 }
