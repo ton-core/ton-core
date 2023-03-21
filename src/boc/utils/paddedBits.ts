538aa5cfc80821b9e4cd5d465c33cbe466a9e672
Ayrat Badykov <ayratin555@gmail.com>
2023-03-21 17:57:45 +0200

    chore: improve the bitsToPaddedBuffer function

--- src/boc/utils/paddedBits.ts
+++ src/boc/utils/paddedBits.ts
@@ -10,13 +10,13 @@ import { BitBuilder } from "../BitBuilder";
 import { BitString } from "../BitString";
 
 export function bitsToPaddedBuffer(bits: BitString) {
-
+    let complete_byte_bits = Math.ceil(bits.length / 8) * 8;
     // Create builder
-    let builder = new BitBuilder(Math.ceil(bits.length / 8) * 8);
+    let builder = new BitBuilder(complete_byte_bits);
     builder.writeBits(bits);
 
     // Apply padding
-    let padding = Math.ceil(bits.length / 8) * 8 - bits.length;
+    let padding = complete_byte_bits - bits.length;
     for (let i = 0; i < padding; i++) {
         if (i === 0) {
             builder.writeBit(1);
